8a748b1fa73e31018b213c60e9f17fefa49714f8
sgr-ksmt <melodydance.k.s@gmail.com>
2017-01-18 17:23:14 +0900

    setup workspace, framework project and demo project.

---
+++ Sources/FlexibleArrow.h
@@ -0,0 +1,19 @@
+//
+//  FlexibleArrow.h
+//  FlexibleArrow
+//
+//  Created by Suguru Kishimoto on 1/18/17.
+//  Copyright © 2017 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexibleArrow.
+FOUNDATION_EXPORT double FlexibleArrowVersionNumber;
+
+//! Project version string for FlexibleArrow.
+FOUNDATION_EXPORT const unsigned char FlexibleArrowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexibleArrow/PublicHeader.h>
+
+
